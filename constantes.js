99f0d10ef6e05e9ac601616ff714fc1422db3e2b
Kevin Arturo <castlestorm0@gmail.com>
2024-02-03 19:42:20 -0600

    cambio de constantes

--- constantes.js
+++ constantes.js
@@ -1,5 +1,5 @@
 //Constantes, valores que una vez declarados no se puede cambiar su valor, por buenas practicas van en mayusculas y se separan las palabras por un guion bajo.
-const MI_CONSTANTE = 44;
+const MI_CONSTANTE = 3.1416;
 
 //MI_CONSTANTE = 25; No podemos cambiar el valor a una constante
 console.log(MI_CONSTANTE);
