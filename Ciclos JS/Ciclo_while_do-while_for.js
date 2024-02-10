7213f879e9648f0b067c0ed6aaef21d2a77a6812
Kevin Arturo <castlestorm0@gmail.com>
2024-02-10 00:41:56 -0600

    Ciclos en JS

--- Ciclos JS/Ciclo_while_do-while_for.js
+++ Ciclos JS/Ciclo_while_do-while_for.js
@@ -1,15 +1,21 @@
-//Ciclo while 
-//Imprimir los valores del 0 al 5;
 let contador = 5, repeticiones = 0;
 
-while(contador >= repeticiones){
+//Ciclo WHILE 
+//Imprimir los valores del 0 al 5;
+/*while(contador >= repeticiones){
     console.log(`${contador--}`);
     //contador++;
-}
+}*/
 
 
-//Ciclo do while, (Se ejecuta al menos una vez el bloque de codigo)
-contador = 5;
+//Ciclo DO WHILE, (Se ejecuta al menos una vez el bloque de codigo)
 do{
     console.log(contador--)
 }while(contador >= repeticiones);
+
+
+//Ciclo FOR, (el contador se declara dentro, ademas del incremento)
+let b = 5;
+for(let a = 0; a <= b; a++){
+    console.log(a);
+}
