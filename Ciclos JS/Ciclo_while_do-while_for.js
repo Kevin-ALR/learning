e1ee4ad6e79b32685adc609daf32bfa8149adfbe
Kevin Arturo <castlestorm0@gmail.com>
2024-02-10 00:02:55 -0600

    ciclos JS

---
+++ Ciclos JS/Ciclo_while_do-while_for.js
@@ -0,0 +1,15 @@
+//Ciclo while 
+//Imprimir los valores del 0 al 5;
+let contador = 5, repeticiones = 0;
+
+while(contador >= repeticiones){
+    console.log(`${contador--}`);
+    //contador++;
+}
+
+
+//Ciclo do while, (Se ejecuta al menos una vez el bloque de codigo)
+contador = 5;
+do{
+    console.log(contador--)
+}while(contador >= repeticiones);
